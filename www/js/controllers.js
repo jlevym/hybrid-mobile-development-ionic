3e2cf9a2c34320c1d8b969bf38a096617234f54f
4lergo <jlevym@gmail.com>
2016-08-09 22:23:28 +0300

    implement About Us view
    second part of assignment 1

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -179,7 +179,8 @@ angular.module('conFusion.controllers', [])
                     }])
 
         .controller('AboutController', ['$scope', 'corporateFactory', 'baseURL', function($scope, corporateFactory, baseURL) {
-            
+                    
+                    $scope.baseURL = baseURL;
                     $scope.leaders = corporateFactory.query();
                     console.log($scope.leaders);
             
